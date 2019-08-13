af020ebcf5b362ffcd65ceebeaa40b47332b2238
Melvyn <melvyn.malherbe@qoqa.com>
2019-08-13 14:24:11 +0200

    QO-5439 - Restructuration and change how note is display
    QO-5439 - Can send note and see after note on website
    
    QO-5439 - Try to add localStorage fonction but dont work right now
    
    QO-5439 - Try to have no conflicts
    
    QO-5438 - Link VUE.js with js
    
    QO-5438 - Add git ignore
    
    QO-5438 - Changement mineur
    
    QO-5438 - Remove desktop.ini
    
    QO-5438 - Change space
    
    QO-5438 - Remove test VUE.JS
    
    QO-5439 - Restructuration and changes how Notes is display

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-
-
+//utile
+msgSendNote = function() {
+  infoAfterSend.styleObject.top = "10%"
+  infoAfterSend.seen = true;
+  setTimeout(function () { infoAfterSend.styleObject.top = '-600px' }, 2000);
+  setTimeout(function () { infoAfterSend.seen = false; }, 2000);
+}
+//variable
+let infoAfterSendMsg;
+let message23 = "";
+//vue.js
+const listNote = new Vue ({
+  el: "#listNote",
+  data: {
+    listNotes: [{ titre: "aucun sens", note: "Pourquoi ce n'est pas possible d'aller à l'école quand je pense que les enfants n'aime pas les chaise mais si il le pense moi je le pense"}, { titre: "l'envie des pouls", note: "si les poules avait des dents je pense que je souhaiterais devenir une poule"}, { titre: "1 éléphant grand", note: "Lorsque je suis aller à la maison\n j'ai vue un éléphant."}, { titre: "le secret de jane", note: "la première fois que j'ai vue JANE je pensais pas pouvoir l'aime"}],
+  }
+});
 const createNote = new Vue({
   el: "#createNote",
   data: {
-    rows: 3,  
-    message: "",    
+    rows: 3, 
+    message: "",
+    titre: "",
   },
   methods: {
     change: function (event) {
-      console.log(this.message);
+      messageSplit = this.message.split("\n");
+      messageSplitLength = messageSplit.length;
+      if (messageSplitLength > 2) {
+        this.rows = messageSplitLength + 1;
+      }
+    },
+    clickSendNote: function () {
+      if (this.titre.length > 3 && this.titre.length < 30) {
+        if (this.message.length > 5 && this.message.length < 2000) {
+          listNote.listNotes.push({ titre : this.titre, note : this.message});
+          console.log("it's pushed");
+          this.message = "";
+          this.titre = "";
+          msgSendNote();
+          infoAfterSend.infoAfterSendMsg = "La note à été sauvegarder.";
+        }
+        else {
+          infoAfterSend.infoAfterSendMsg = "Merci d'effectuer une note de plus de 5 caractères. 2000 est la limites.";
+          msgSendNote();
+        }
+      }
+      else {
+        infoAfterSend.infoAfterSendMsg = "Merci de donner un titre de plus de 3 caractères et un maximume de 30.";
+        msgSendNote();
+      }
     }
   }
 })
-
-const app = new Vue({
-  el: "#app",
+const infoAfterSend = new Vue({
+  el: "#infoAfterSend",
   data: {
-    message: "salut",
+    seen: false,
+    infoAfterSendMsg: "La note à été sauvegarder.",
+    styleObject: {
+      top: '-600px'
+    }
   }
 })
