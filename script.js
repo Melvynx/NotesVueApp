fc2e6e34330b319e512f3f3c95fb8ccde08a0713
Melvyn <melvyn.malherbe@qoqa.com>
2019-08-15 10:43:21 +0200

    QO-5442 - Afficher un bouton pour supprimer une note

--- script.js
+++ script.js
@@ -34,20 +34,37 @@ let message23 = "";
 const listNote = new Vue ({
   el: "#listNote",
   data: {
-    listNotes: [{ id: 0, titre: "aucun sens", note: "Pourquoi ce n'est pas possible d'aller à l'école quand je pense que les enfants n'aime pas les chaise mais si il le pense moi je le pense"}, {  id: 1, titre: "l'envie des pouls", note: "si les poules avait des dents je pense que je souhaiterais devenir une poule"}, { id: 2, titre: "1 éléphant grand", note: "Lorsque je suis aller à la maison\n j'ai vue un éléphant."}, { id: 3, titre: "le secret de jane", note: "la première fois que j'ai vue JANE je pensais pas pouvoir l'aime"}],
+    listNotes: [{id:"0", titre: 'Salut', note:'blabla'},{ id: "1", titre: "aucun sens", note: "Pourquoi ce n'est pas possible d'aller à l'école quand je pense que les enfants n'aime pas les chaise mais si il le pense moi je le pense"}, {  id: "2", titre: "l'envie des pouls", note: "si les poules avait des dents je pense que je souhaiterais devenir une poule"}, { id: "3", titre: "1 éléphant grand", note: "Lorsque je suis aller à la maison\n j'ai vue un éléphant."}, { id: "4", titre: "le secret de jane", note: "la première fois que j'ai vue JANE je pensais pas pouvoir l'aime"}],
     seen: true,
-  
-  
   },
   methods: {
-    overDone: function (event) {
-      event.target.getElementsByClassName("buttonDelete")[0].classList.remove("hideButton")
-      console.log("l'oiseau est la");
-      console.log(event.target.getAttribute("data-id"));
+    mouseOver: function (event) {
+      const children = event.currentTarget.children;
+      // Toujours le dernier élément du tableau
+      const deleteButton = children[children.length - 1];
+
+      if(deleteButton) {
+        deleteButton.classList.remove("hideButton");
+      }
     },
-    overFalse: function () {
-      event.target.getElementsByClassName("buttonDelete")[0].classList.add("hideButton")
-      console.log("il est partie")
+    mouseLeave: function (event) {
+      const children = event.currentTarget.children;
+      // Toujours le dernier élément du tableau
+      const deleteButton = children[children.length - 1];
+      
+      if(deleteButton) {
+        deleteButton.classList.add("hideButton");
+      }
+    },
+    deleteNote: function(event) {
+      const parentElement = event.currentTarget.parentElement;
+      const id = parentElement.getAttribute('data-id');
+
+      const index = listNote.listNotes.findIndex(note => note.id === id);
+      
+      if(index > -1) {
+        listNote.listNotes.splice(index, 1);
+      }
     }
   }
 });
@@ -69,7 +86,7 @@ const createNote = new Vue({
     clickSendNote: function () {
       if (checkedTitle(this.titre) && checkedNote(this.message)) {
         
-        listNote.listNotes.push({ id: findTheBiggestID(), titre : this.titre, note : this.message});
+        listNote.listNotes.push({ id: String(findTheBiggestID()), titre : this.titre, note : this.message});
         console.log("it's pushed");
         this.message = "";
         this.titre = "";
